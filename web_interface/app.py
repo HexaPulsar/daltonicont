6519e465f2d917bf081ccb2e999daa32f03580d8
Diego Torreblanca <diego.torreblanca77@gmail.com>
2023-06-09 22:32:47 -0400

    DT Add: added parameters page
    Agregué la página de parámetros de ejemplo para luego tomarlos como argumentos de la función. Además, añadí el archivo estilos.css para darle estilo jej

--- web_interface/app.py
+++ web_interface/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, redirect, url_for, send_file, abort
+from flask import Flask, render_template, redirect, url_for, send_file, abort, request
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
 
-def write_text(filename):
+def write_text(filename, msg):
     with open(filename, 'w') as f:
-        f.write('readme')
+        f.write(msg)
         f.close()
 
 app = Flask(__name__)
@@ -33,14 +33,21 @@ def home():
         if file_ext not in app.config['UPLOAD_EXTENSIONS']: # Check if file extention is allowed
             abort(400)
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER_FILES'],secure_filename(app.config['MODIFIED_FILENAME']))) # Then save the file
-
-        directory = app.config['UPLOAD_FOLDER_FILES'] + app.config['MODIFIED_FILENAME'] # Get the saved file
-        write_text(directory) # Modify it with the function
-        print("File has been uploaded and edited.")
-
-        return redirect(url_for('result')) # Go to result.html
+        return redirect(url_for('parameters')) # Go to param.html
     else:
         return render_template('index.html', file_form=formulario)
+
+@app.route('/parameters', methods=['GET',"POST"])
+def parameters():
+    if request.method == 'POST':
+        box = request.form.getlist('preferences') # All checkboxes values stored in array of strings
+        mensaje = "".join(box)
+        directory = app.config['UPLOAD_FOLDER_FILES'] + app.config['MODIFIED_FILENAME'] # Get the saved file
+        write_text(directory,mensaje) # Modify it with the function
+        print("File has been uploaded and edited.")
+        return redirect(url_for('result')) # Go to param.html
+    else:
+        return render_template('param.html')
 
 @app.route('/about')
 def about_us():
