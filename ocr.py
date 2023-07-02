fccad6102c6b21f761acb71924e1b34528675c53
Fernanda Borja <102128933+feborja@users.noreply.github.com>
2023-07-02 17:10:57 -0400

    Updated ocr module
    Now the file is almost as heavy as the original :s

--- ocr.py
+++ ocr.py
@@ -6,7 +6,10 @@ For now the files gets heavier xdxxdxdxddddd
 TODO: !!!! ESTUDIAR LA LIBRERIA, SE SUPONE QUE PUEDE DEJAR MENOS PESADOS LOS ARCHIVOS
 '''
 # Import libraries
+# Se necesita unpaper
 import ocrmypdf
 # Basic execution
 #                                         lo deje aqui owo
-ocrmypdf.ocr('output\\pdf\\archivo.pdf', 'output\\pdf\\OCR_output.pdf', deskew=True)
+# ocrmypdf.configure_logging()
+# , deskew=True
+ocrmypdf.ocr('output\\pdf\\archivo.pdf', 'output\\pdf\\OCR_output.pdf', output_type = "pdf", optimize = 1)
