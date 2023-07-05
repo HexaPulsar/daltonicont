42e6517cb6805aa8853aba444fae3097d7a43caf
Fernanda Borja <102128933+feborja@users.noreply.github.com>
2023-07-04 20:35:46 -0400

    Now OCR in main :D

--- ocr.py
+++ ocr.py
@@ -2,11 +2,11 @@
 OCR MODULE
 See https://ocrmypdf.readthedocs.io/en/latest/installation.html
 to install, in windows it required chocolatey
-For now the files gets heavier xdxxdxdxddddd
+For now the files gets heavier 
 TODO: !!!! ESTUDIAR LA LIBRERIA, SE SUPONE QUE PUEDE DEJAR MENOS PESADOS LOS ARCHIVOS
 '''
 # Import libraries
 import ocrmypdf
 # Basic execution
 #                                         lo deje aqui owo
-ocrmypdf.ocr('output\\pdf\\archivo.pdf', 'output\\pdf\\OCR_output.pdf', deskew=True)
+ocrmypdf.ocr('output\\pdf\\archivo.pdf', 'output\\pdf\\OCR_output.pdf', output_type = "pdf", optimize = 1)
